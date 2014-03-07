273b52342ce8ff3daf18122704bf6b53a800357e
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-03-07 15:26:47 -0600

    update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
     @user = current_user
-    @events = @user.created_events.to_json
+    @events = @user.created_events
+  end
+
+  def get_events 
+    events = {}
+    current_user.created_events.each_with_index do |event, index|
+      events[index] = event.to_json
+    end
+    events.to_json
   end
 end
